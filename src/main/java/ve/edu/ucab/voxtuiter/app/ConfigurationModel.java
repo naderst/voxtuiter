271496157bbfec492152308d048476cc2861bad8
Nader Abu Fakhr <naderst@gmail.com>
2014-04-16 12:30:09 -0430

    Añadido archivo modelo para la configuración

---
+++ src/main/java/ve/edu/ucab/voxtuiter/app/ConfigurationModel.java
@@ -0,0 +1,17 @@
+/*
+    Vox Tuiter
+
+    Desarrollado por:
+        - Nader Abu Fakhr (@naderst)
+        - Moisés Moussa (@mdsse)
+
+    GitHub: https://github.com/naderst/voxtuiter
+
+    UCAB Guayana - Puerto Ordaz, Edo Bolívar. Venezuela
+ */
+package ve.edu.ucab.voxtuiter.app;
+
+public class ConfigurationModel {
+    final public static String consumerKey = "";
+    final public static String consumerSecret = "";
+}
